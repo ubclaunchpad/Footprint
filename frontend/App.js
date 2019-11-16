0d7fe3b5a1fdc23a22d2161adb09b0232d7d8f05
Achinth Bharadwaj <achinth.bharadwaj@gmail.com>
2019-11-16 15:44:22 -0800

    Added viz.js with premative vizualizations

--- frontend/App.js
+++ frontend/App.js
@@ -1,19 +1,10 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import Viz from "./screens/DataScreen/viz";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <Viz />
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
