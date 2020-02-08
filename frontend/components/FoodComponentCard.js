ee0f90e1d7812a33338ac43afa4c16a701d08ffc
Odin <odin_mebesius@yahoo.ca>
2020-02-08 13:28:46 -0800

    product-info UI

---
+++ frontend/components/FoodComponentCard.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
+import Red from '../assets/red-bg.svg';
+import Blue from '../assets/blue-bg.svg';
+
+/* The FoodComponentCard class is a custom view that displays what 
+ * is in the food item a user has scanned. 
+ * Props: type - the type of food ("meat", "dairy", "fish", "veg", "fruit", or "grains")
+ *        icon - the icon representing the type of food as an svg
+ * 
+ * State: count - the # of times an item belonging to one type appeared
+ *                  in what the user scanned
+ */
+export default class FoodComponentCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: "0"
+        };
+      }
+    
+    render() {
+        const redBackground = <Red style={styles.background} />
+        const blueBackground = <Blue width={'100%'} height={'100%'} />
+        return (
+            <View style = {{width: '100%', height: '100%'}}>
+                <View style = {{position: 'absolute', left: 0, top: 0, height: '100%', width: '100%', justifyContent: 'center', alignItems: 'center'}}>
+                    <View style = {{width: '85%', height: '85%', alignItems: 'center'}}>
+                        {this.props.color === 'red' ? redBackground : blueBackground}
+                    </View>
+                </View>
+                <View style = {{position: 'absolute', left: 0, top: 0, width: '100%', height: '100%', flexDirection: 'row'}}>
+                    <View style = {{flex: 1}}>
+                    </View>
+                    <View style = {{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+                        <View style = {{flex: 5, justifyContent: 'flex-end'}}>
+                            <Text style={styles.count}>
+                                {this.state.count}
+                            </Text>
+                        </View>
+                        <View style = {{flex: 6, justifyContent: 'flex-start', paddingTop: 5}}>
+                            <Text style={styles.type}>
+                                {this.props.type}
+                            </Text>
+                        </View>
+                    </View>
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    baseText: {
+      fontFamily: 'Cochin',
+    },
+    count: {
+      fontSize: 40,
+      fontWeight: 'bold',
+      color: '#FFFFFF',
+    },
+    type: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#FFFFFF',
+    },
+    background: {
+        width: '100%',
+        height: '100%',
+    },
+  });
--- frontend/screens/ProductInfoScreen/index.js
+++ frontend/screens/ProductInfoScreen/index.js
@@ -1,35 +1,104 @@
 import React from 'react';
 import {StyleSheet, Text, View, Button} from 'react-native';
+import FoodComponentCard from '../../components/FoodComponentCard';
+import splash from '../../assets/splash.png';
 
 export default function ProductInfoScreen(props) {
-  const { navigate, getParam } = props.navigation;
-  const response = getParam("response", "OOPS");
+  //const { navigate, getParam } = props.navigation;
+  //const response = getParam("response", "OOPS");
+  const total = 'Total\nCO2\nFootprint:';
+  const units = 'kg CO2';
+  const magnitude = 22;
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>{response}</Text>
-        <Text style={styles.text}>Product Info Screen</Text>
-        <Button title="Analytics" onPress={() => navigate('Analytics')} />
-
+      <View style={[styles.elementsContainer]}>
+        <View style={{flex: 6, flexDirection: 'row'}}>
+          <View style={{flex: 3}}>
+            <View style = {{flex: 1}}>
+              <FoodComponentCard type='Dairy' color = 'red'/>
+            </View>  
+            <View style = {{flex: 1}}>
+              <FoodComponentCard type='Meat' color = 'red'/>
+            </View>
+            <View style = {{flex: 1}}>
+              <FoodComponentCard type='Fish' color = 'red'/>
+            </View>
+          </View>
+          <View style={{flex: 3}}>
+            <View style = {{flex: 1}}>
+              <FoodComponentCard type='Grains' color = 'blue'/>
+            </View>
+            <View style = {{flex: 1}}>
+              <FoodComponentCard type='Fruit' color = 'blue'/>
+            </View>
+            <View style = {{flex: 1}}>
+              <FoodComponentCard type='Veg' color = 'blue'/>
+            </View>
+          </View>
+        </View>
+        <View style={{flex: 4}}>
+          <View style = {{flex: 3, flexDirection: 'row'}}>
+            <View style = {{flex: 1, justifyContent: 'center', alignItems: 'flex-end', paddingBottom: 10, paddingRight: 5}}>
+              <Text style ={styles.textTotal}>
+                {total}
+              </Text>
+            </View>
+            <View style = {{flex: 1}}>
+              <View style = {{flex: 1, justifyContent: 'flex-end', paddingLeft: 15}}>
+                <Text style ={styles.magnitude}>
+                  {magnitude}
+                </Text>
+              </View>
+              <View style = {{flex: 1, paddingLeft: 15, paddingTop: 10}}>
+                <Text style ={styles.units}>
+                  {units}
+                </Text>
+              </View>
+            </View>
+          </View>  
+          <View style = {{flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center', backgroundColor: '#FFFFFF'}}>
+            <Button title = 'Nihao' color = '#FF9786'/>
+            <Button title = 'Hello' color = '#FF9786'/>
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
 
 ProductInfoScreen.navigationOptions = {
-  title: 'Product Info',
+  title: 'Footprint Components',
 };
 
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    text: {
-      fontSize: 17,
-      color: 'rgba(96,100,109, 1)',
-      lineHeight: 24,
-      textAlign: 'center',
-    },
-});
+const styles = {
+  textTotal: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    lineHeight: 30,
+  },
+  magnitude: {
+    fontSize: 52,
+    fontWeight: 'bold',
+  },
+  units: {
+    fontSize: 28,
+    fontWeight: 'bold',
+  },
+  container: {
+    marginTop: 48,
+    flex: 1
+  },
+  headerStyle: {
+    fontSize: 24,
+    textAlign: 'center',
+    fontWeight: '100',
+    marginBottom: 24
+  },
+  elementsContainer: {
+    flex: 1,
+    marginLeft: 24,
+    marginRight: 24,
+    marginBottom: 24
+  }
+}
   
