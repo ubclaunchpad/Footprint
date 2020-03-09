0b6571900eb77d95e114913caf163799d87fc5ae
Odin <odin_mebesius@yahoo.ca>
2020-03-09 13:19:09 -0700

    sign in with webview working, need to add authentication flow

--- frontend/App.js
+++ frontend/App.js
@@ -1,9 +1,14 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import AnalyticsScreen from "./screens/AnalyticsScreen";
-import CameraScreen from "./screens/CameraScreen";
+import AnalyticsScreen from './screens/AnalyticsScreen';
+import CameraScreen from './screens/CameraScreen';
 import ProductInfoScreen from './screens/ProductInfoScreen';
+import LoginScreen from './screens/LoginScreen';
+import ProfileScreen from './screens/ProfileScreen';
+
 const MainNavigator = createStackNavigator({
+  Login: { screen: LoginScreen },
+  Profile: { screen: ProfileScreen },
   Analytics: { screen: AnalyticsScreen },
   Camera: { screen: CameraScreen },
   ProductInfo: {screen: ProductInfoScreen}
---
+++ frontend/screens/ProfileScreen/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { Text, View, StyleSheet, Button } from "react-native";
+
+export default class ProfileScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text> Profile Screen </Text>
+        <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+          Welcome, {this.props.navigation.getParam("username")}
+        </Text>
+        <Button
+          title="Sign out"
+          onPress={() => this.props.navigation.navigate("Login")}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
